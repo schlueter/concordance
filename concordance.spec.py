138e9a588f5b433e3b6cc86b5838e4237f01948f
Schlueter <bs@bschlueter.com>
2015-02-20 04:42:50 -0500

    Conform test data to defined classes

--- concordance.spec.py
+++ concordance.spec.py
@@ -9,43 +9,46 @@ import concordance
 class TestConcordance(unittest.TestCase):
 
     def setUp(self):
-        self.test_book = book.Book([
-            '''To be, or not to be- that is the question:
+        self.test_book = book.Book(
+            'An excerpt from Hamlet',
+            [
+                '''To be, or not to be- that is the question:
 Whether 'tis nobler in the mind to suffer''',
-            '''The slings and arrows of outrageous fortune
+                '''The slings and arrows of outrageous fortune
 Or to take arms against a sea of troubles,''',
-            '''And by opposing end them. To die- to sleep-
+                '''And by opposing end them. To die- to sleep-
 No more; and by a sleep to say we end''',
-            '''The heartache, and the thousand natural shocks
+                '''The heartache, and the thousand natural shocks
 That flesh is heir to. 'Tis a consummation''',
-            '''Devoutly to be wish'd. To die- to sleep.
+                '''Devoutly to be wish'd. To die- to sleep.
 To sleep- perchance to dream: ay, there's the rub!''',
-            '''For in that sleep of death what dreams may come
-When we have shuffled off this mortal coil, ''',
-            '''Must give us pause. There's the respect
+                '''For in that sleep of death what dreams may come
+When we have shuffled off this mortal coil,''',
+                '''Must give us pause. There's the respect
 That makes calamity of so long life.''',
-            '''For who would bear the whips and scorns of time,
+                '''For who would bear the whips and scorns of time,
 Th' oppressor's wrong, the proud man's contumely,''',
-            '''The pangs of despis'd love, the law's delay,
+                '''The pangs of despis'd love, the law's delay,
 The insolence of office, and the spurns''',
-            '''That patient merit of th' unworthy takes,
+                '''That patient merit of th' unworthy takes,
 When he himself might his quietus make''',
-            '''With a bare bodkin? Who would these fardels bear,
-To grunt and sweat under a weary life, ''',
-            '''But that the dread of something after death-
+                '''With a bare bodkin? Who would these fardels bear,
+To grunt and sweat under a weary life,''',
+                '''But that the dread of something after death-
 The undiscover'd country, from whose bourn''',
-            '''No traveller returns- puzzles the will,
+                '''No traveller returns- puzzles the will,
 And makes us rather bear those ills we have''',
-            '''Than fly to others that we know not of?
+                '''Than fly to others that we know not of?
 Thus conscience does make cowards of us all,''',
-            '''And thus the native hue of resolution
+                '''And thus the native hue of resolution
 Is sicklied o'er with the pale cast of thought,''',
-            '''And enterprises of great pith and moment
-With this regard their currents turn awry ''',
-            '''And lose the name of action.- Soft you now!
+                '''And enterprises of great pith and moment
+With this regard their currents turn awry''',
+                '''And lose the name of action.- Soft you now!
 The fair Ophelia!- Nymph, in thy orisons''',
-            '''Be all my sins rememb'red.'''
-        ])
+                '''Be all my sins rememb'red.'''
+              ]
+        )
         self.test_book_concordance_data = {
             '\'tis': set([0, 3]),
             'a': set([1, 2, 3, 10]),
@@ -220,13 +223,16 @@ The fair Ophelia!- Nymph, in thy orisons''',
             'you': set([16])
         }
 
-        self.simple_book = book.Book([
-            'Apple Banana',
-            'Apple',
-            'Apple Zebra',
-            '',
-            'Banana'
-        ])
+        self.simple_book = book.Book(
+            'A very simple book',
+            [
+                'Apple Banana',
+                'Apple',
+                'Apple Zebra',
+                '',
+                'Banana'
+            ]
+        )
 
         self.simple_book_concordace_data = {
             'Apple': [1,2,3],
